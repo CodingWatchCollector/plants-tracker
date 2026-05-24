0066af56bf818d652ba4ed31f5fc323b722d4626
Anton Begunenko <abegunenko@laprovence.com>
2026-05-24 14:39:48 +0200

    test env

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,16 @@
 
 import type { Home } from '$lib/types';
 
+type ENV = {
+	TURSO_URL: string;
+	TURSO_AUTH_TOKEN: string;
+	VAPID_KEYS: string;
+	VAPID_SUBJECT: string;
+	PUBLIC_APP_URL: string;
+	PLANT_IMAGES: R2Bucket;
+	JWT_SECRET: string;
+	PUBLIC_VAPID_PUBLIC_KEY: string;
+};
 // for information about these interfaces
 declare global {
 	namespace App {
@@ -9,16 +19,7 @@ declare global {
 			home: Pick<Home, 'id' | 'name'> | null;
 		}
 		interface Platform {
-			env: {
-				TURSO_URL: string;
-				TURSO_AUTH_TOKEN: string;
-				VAPID_KEYS: string;
-				VAPID_SUBJECT: string;
-				PUBLIC_APP_URL: string;
-				PLANT_IMAGES: R2Bucket;
-				JWT_SECRET: string;
-				PUBLIC_VAPID_PUBLIC_KEY: string;
-			};
+			env: ENV;
 			cf: CfProperties;
 			ctx: ExecutionContext;
 		}
@@ -35,6 +36,9 @@ declare global {
 	interface ScheduledEvent {
 		scheduledTime: number;
 		cron: string;
+	}
+	namespace NodeJS {
+		type ProcessEnv = ENV;
 	}
 }
 
--- src/lib/vapid.ts
+++ src/lib/vapid.ts
@@ -1,7 +1,6 @@
-import { PUBLIC_VAPID_PUBLIC_KEY } from '$env/static/public';
-
 export function getVapidPublicKey(): ArrayBuffer {
-	const base64 = PUBLIC_VAPID_PUBLIC_KEY.replace(/-/g, '+').replace(/_/g, '/');
+	console.log('VAPID_PUBLIC_KEY', process.env.PUBLIC_VAPID_PUBLIC_KEY);
+	const base64 = (process.env.PUBLIC_VAPID_PUBLIC_KEY || '').replace(/-/g, '+').replace(/_/g, '/');
 	const raw = atob(base64);
 	return Uint8Array.from([...raw].map((c) => c.charCodeAt(0))).buffer;
 }
